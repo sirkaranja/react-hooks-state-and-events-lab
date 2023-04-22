05bed1af5de9483da928b653f9eaf4e856a77ce0
Daniel Karanja <sirkaranja12@gmail.com>
2023-04-22 13:01:32 +0300

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [addItem, setAddItem] = useState (false)
+
+  function onAddItem () {
+    setAddItem ((addItem)=> !addItem)
+  }
+
+  // const myItem = addItem ? "in-cart": ""
+  const color = addItem ? "purple" : ""
+
   return (
-    <li className="">
+    <li className= {addItem ? "in-cart": ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick= {onAddItem} style = {{background : color}} className="add">{addItem ? "Remove from cart": "Add to cart"}</button>
     </li>
   );
 }
